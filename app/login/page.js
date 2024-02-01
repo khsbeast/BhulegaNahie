74441ba52599a0cf465fd29c930993cef37aa912
Kumar Harsh <harsh4981@gmail.com>
2024-02-02 00:35:29 +0530

    go

--- app/login/page.js
+++ app/login/page.js
@@ -1,9 +1,15 @@
+"use client"
+
 import Logo from "../../Assets/logo.svg";
-import ActionMenu from "@/Components/ActionMenu/ActionMenu";
 import countriesData from "./countries";
 import Flag from "../../Assets/flag.svg";
 import Image from "next/image";
 import Link from "next/link";
+import React, { useState } from "react";
+import FileIcon from "../../Assets/file.jsx";
+import InfoCircle from "../../Assets/info-circle.jsx";
+import Happy from "../../Assets/happy.jsx";
+
 
 const countries = (
   <select className="h-6 w-14 outline-none border-r pr-1" defaultValue="IN">
@@ -55,7 +61,49 @@ const Page = () => {
             </div>
           </section>
           <section className="flex justify-center mt-10">
-            <ActionMenu />
+            const [claimHover, setClaimHover] = useState(false); const
+            [surveyHover, setSurveyHover] = useState(false); return (
+            <div className="w-[420px]">
+              <button
+                onMouseLeave={() => setClaimHover(false)}
+                onMouseEnter={() => setClaimHover(true)}
+                className="flex justify-between items-center bg-white text-sm font-semibold uppercase text-black border border-blueprimary pl-4 w-full h-[46px] rounded mt-10 leading-5 hover:bg-blueprimary hover:border-blueprimary hover:opacity-100 hover:border-opacity-100"
+              >
+                <div className="flex gap-3 items-center">
+                  <FileIcon color={claimHover ? "#fff" : "#3361E1"} />
+                  <h2
+                    className="text-blueprimary text-sm leading-5 font-semibold"
+                    style={
+                      claimHover ? { color: "#fff" } : { color: "#3361E1" }
+                    }
+                  >
+                    Claim assistance
+                  </h2>
+                </div>
+                <InfoCircle
+                  color={claimHover ? "#fff" : "#000"}
+                  className="mr-4"
+                />
+              </button>
+              <button
+                onMouseLeave={() => setSurveyHover(false)}
+                onMouseEnter={() => setSurveyHover(true)}
+                className="mt-4 flex justify-between items-center bg-white text-sm font-semibold uppercase text-black border border-blueprimary pl-4 w-full h-[46px] rounded leading-5 hover:bg-blueprimary hover:border-blueprimary hover:opacity-100 hover:border-opacity-100"
+              >
+                <div className="flex gap-3 items-center">
+                  <Happy color={surveyHover ? "#fff" : "#3361E1"} />
+                  <h2
+                    className="text-blueprimary text-sm leading-5 font-semibold"
+                    style={
+                      surveyHover ? { color: "#fff" } : { color: "#3361E1" }
+                    }
+                  >
+                    Take the Survey
+                  </h2>
+                </div>
+                <InfoCircle color={surveyHover ? "#fff" : "#000"} />
+              </button>
+            </div>
           </section>
         </div>
       </div>
